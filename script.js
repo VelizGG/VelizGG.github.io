251196b0272cede09ba1a8aba3aaf3ea9715dc32
Gabriel Veliz <velizindustries@gmail.com>
2024-06-02 01:16:25 -0600

    Initial commit

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 async function obtenerUbicacion() {
-    try {
-
-        // Mostrar la imagen con el texto
-        const imagenContainer = document.getElementById('imagen-container');
-        imagenContainer.innerHTML = ''; // Limpiar contenido previo
-        const imagen = document.createElement('img');
-        imagen.src = 'images/tania.jpg';
-        imagen.alt = 'Imagen de Tania';
-        const texto = document.createElement('p');
-        texto.textContent = 'Te amo demasiado, amor';
-        imagenContainer.appendChild(imagen);
-        imagenContainer.appendChild(texto);
+    try {      
+        // Redireccionar a la página cartaamor.html
+        window.location.href = 'pages/cartaamor.html';
 
     } catch (error) {
-        alert('No se pudo obtener la ubicación: ' + error);
+        alert('No se pudo obtener la ubicación: ' + error.message);
     }
 }
